a01b8d6fdb540dc286c4b087407a6487d29ce257
Shmulik <shmulike@post.bgu.ac.il>
2020-04-23 18:53:52 +0300

    CNC simplify

--- infi_navigation/config/nav_parameters_carto.lua
+++ infi_navigation/config/nav_parameters_carto.lua
@@ -24,7 +24,7 @@ options = {
   odom_frame = "odom",
   provide_odom_frame = false,
   publish_frame_projected_to_2d = false,
-  use_odometry = false,
+  use_odometry = true,
   use_nav_sat = false,
   use_landmarks = false,
   num_laser_scans = 1,
@@ -35,10 +35,10 @@ options = {
   submap_publish_period_sec = 0.3,
   pose_publish_period_sec = 5e-3,
   trajectory_publish_period_sec = 30e-3,
-  rangefinder_sampling_ratio = 1.,
-  odometry_sampling_ratio = 1.,
+  rangefinder_sampling_ratio = 0.1,
+  odometry_sampling_ratio = 0.1,
   fixed_frame_pose_sampling_ratio = 1.,
-  imu_sampling_ratio = 1.,
+  imu_sampling_ratio = 0.005,
   landmarks_sampling_ratio = 1.,
 }
 
